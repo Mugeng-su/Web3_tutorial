e0c53cda1804b407b1077192ffde9c920f803df8
Mugeng Su <mugengsu@gmail.com>
2025-05-04 13:22:17 +0800

    finish lesson 4

--- scripts/deployFundMe.js
+++ scripts/deployFundMe.js
@@ -14,8 +14,51 @@ async function main(){
     await fundMe.waitForDeployment() // 等待合约部署完成
     console.log(`contract has been deployed successfully, contract address is:  ${fundMe.target}`); 
     // 打印合约地址，并且使用模板字符串，美元符号后面加上大括号，表示要插入变量的值
-}
+    
 
+    // verify fundme
+    if(hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for 5 confirmations")
+        await fundMe.deploymentTransaction().wait(5) 
+        await verifyFundMe(fundMe.target, [300])
+    } else {
+        console.log("verification skipped..")
+    }
+
+    // init 2 accounts
+    const [firstAccount, secondAccount] = await ethers.getSigners()
+
+    // fund contract with first account
+    const fundTx = await fundMe.fund({value: ethers.parseEther("0.5")})
+    await fundTx.wait()
+
+    console.log(`2 accounts are ${firstAccount.address} and ${secondAccount.address}`)
+
+    // check balance of contract
+    const balanceOfContract = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContract}`)
+
+    // fund contract with second account
+    const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({value: ethers.parseEther("0.5")})
+    await fundTxWithSecondAccount.wait()
+
+    // check balance of contract
+    const balanceOfContractAfterSecondFund = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContractAfterSecondFund}`)
+
+    // check mapping 
+    const firstAccountbalanceInFundMe = await fundMe.fundersToAmount(firstAccount.address)
+    const secondAccountbalanceInFundMe = await fundMe.fundersToAmount(secondAccount.address)
+    console.log(`Balance of first account ${firstAccount.address} is ${firstAccountbalanceInFundMe}`)
+    console.log(`Balance of second account ${secondAccount.address} is ${secondAccountbalanceInFundMe}`)
+    }
+
+async function verifyFundMe(fundMeAddr, args) {
+    await hre.run("verify:verify", {
+        address: fundMeAddr,
+        constructorArguments: args,
+    });
+    }
 
 
 main().then().catch((error) => {
